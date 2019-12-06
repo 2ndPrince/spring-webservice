2666824f02fe46dcb84174082cb1047270b04db0
Youngseok Lee <astonisher88@gmail.com>
2019-12-06 13:32:28 -0500

    2

---
+++ src/test/java/com/youngseok/webservice/domain/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.youngseok.webservice.domain;
+
+
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+	@Autowired
+	PostsRepository postsRepository;
+	
+	@After
+	public void cleanup() {
+		postsRepository.deleteAll();
+	}
+	
+	@Test
+	public void loading_post() {
+		//given
+		postsRepository.save(Posts.builder()
+					.title("Test Title")
+					.author("Test Author")
+					.content("astonisher88")
+					.build());
+		
+		//when
+		List<Posts> postsList = postsRepository.findAll();
+		
+		//then
+		Posts posts = postsList.get(0);
+		assertThat(posts.getTitle(), is("Test Title"));
+		assertThat(posts.getContent(), is("astonisher88"));
+		
+	}
+
+}
